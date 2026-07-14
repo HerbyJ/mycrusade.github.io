bd1e3b5baa8ad6158d9f87e21da824d024b66ed5
Ryan Dunn <dunn.ryan.01@gmail.com>
2026-07-13 19:02:23 -0600

    Adding hardcoded yt embedded video for testing

--- js/videos.js
+++ js/videos.js
@@ -101,19 +101,22 @@ function createEmbed(video) {
 
             return `
 
-            <iframe
-
-                src="${video.embed}"
-
-                title="${video.title}"
-
-                frameborder="0"
-
-                allowfullscreen>
-
-            </iframe>
+            
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/laeYgYon0xs?si=45gbfXoUAGWbL8h_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
             `;
+
+            // <iframe
+
+            //     src="${video.embed}"
+
+            //     title="${video.title}"
+
+            //     frameborder="0"
+
+            //     allowfullscreen>
+
+            // </iframe>
 
 
 
